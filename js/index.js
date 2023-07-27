578801254d4dec69008a857bb5b51e981b89c573
krenvp <karenperarnau@gmail.com>
2023-07-27 12:49:29 -0400

    Adding app logic

---
+++ js/index.js
@@ -0,0 +1,42 @@
+import TwoFactorCodeInput from "./two-factor-code-input.mjs";
+
+// DOM Elements
+const twoFactorCodeSection = document.querySelector(".two-factor-code_section");
+const messageSection = document.querySelector(".message_section");
+const inputs = [...document.querySelector(".twc_inputs").children];
+const validateCodeBtn = document.querySelector(".twc_verify-btn");
+const seeValidCodeBtn = document.querySelector(".see-valid-code_btn");
+const validCodeElem = document.querySelector(".valid-code");
+
+const app = new TwoFactorCodeInput(inputs);
+
+seeValidCodeBtn.addEventListener("click", onSeeValidCode);
+validateCodeBtn.addEventListener("click", onValidateCode);
+
+function onSeeValidCode(e) {
+  e.preventDefault();
+
+  validCodeElem.textContent = app.getValidCode();
+  seeValidCodeBtn.classList.add("hidden");
+  validCodeElem.classList.remove("hidden");
+
+  setTimeout(() => {
+    seeValidCodeBtn.classList.remove("hidden");
+    validCodeElem.classList.add("hidden");
+  }, 1500);
+}
+
+function onValidateCode(e) {
+  e.preventDefault();
+
+  if (app.validateCode()) {
+    twoFactorCodeSection.classList.add("hidden");
+    messageSection.classList.remove("hidden");
+  } else {
+    inputs.forEach((input) => {
+      input.classList.add("error");
+
+      setTimeout(() => input.classList.remove("error"), 1500);
+    });
+  }
+}
---
+++ js/two-factor-code-input.mjs
@@ -0,0 +1,103 @@
+export default function TwoFactorCodeInput(inputs) {
+  this.validCode = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000 + "";
+  this.codeToValidate = [];
+  this.isDeleting = false;
+  this.inputs = inputs;
+
+  this.inputs.forEach((input) => {
+    input.addEventListener("input", this.onInputCode.bind(this));
+    input.addEventListener("keyup", this.onInputCode.bind(this));
+    input.addEventListener("paste", this.onPasteCode.bind(this));
+  });
+}
+
+TwoFactorCodeInput.prototype.onInputCode = function (e) {
+  const currentInput = e.currentTarget;
+  const currentInputId = currentInput.id;
+  const currentInputPosition = currentInputId[currentInputId.length - 1] - 1;
+  const enteredDigit = currentInput.value;
+  const isANumber = /\d/.test(enteredDigit);
+  const isBackspacePressed = e.key === "Backspace";
+
+  if (isANumber) {
+    const nextInput = this.inputs[currentInputPosition + 1];
+
+    this.onEnterDigit({
+      currentInput,
+      currentInputPosition,
+      enteredDigit,
+      nextInput,
+    });
+  } else if (isBackspacePressed) {
+    const prevInput = this.inputs[currentInputPosition - 1];
+
+    this.onBackspacePressed({ currentInput, currentInputPosition, prevInput });
+  }
+};
+
+TwoFactorCodeInput.prototype.onEnterDigit = function ({
+  currentInput,
+  currentInputPosition,
+  enteredDigit,
+  nextInput,
+}) {
+  if (nextInput) {
+    currentInput.disabled = true;
+    nextInput.disabled = false;
+    nextInput.focus();
+  }
+
+  this.codeToValidate[currentInputPosition] = enteredDigit;
+  this.isDeleting = false;
+};
+
+TwoFactorCodeInput.prototype.onBackspacePressed = function ({
+  currentInput,
+  currentInputPosition,
+  prevInput,
+}) {
+  if (!this.isDeleting) {
+    this.codeToValidate[currentInputPosition] = "";
+
+    this.isDeleting = true;
+  } else if (prevInput) {
+    currentInput.disabled = true;
+    prevInput.value = this.codeToValidate[currentInputPosition - 1];
+    prevInput.disabled = false;
+    prevInput.focus();
+
+    this.isDeleting = false;
+  }
+};
+
+TwoFactorCodeInput.prototype.onPasteCode = function (e) {
+  e.preventDefault();
+  const isPastedOnFirstInput = e.currentTarget.id === "digit-1";
+
+  if (isPastedOnFirstInput) {
+    const pastedCode = e.clipboardData.getData("text");
+    const isANumber = /\d/.test(pastedCode);
+
+    if (isANumber && pastedCode.length <= this.inputs.length) {
+      pastedCode.split("").forEach((digit, i) => {
+        this.inputs[i].value = digit;
+        this.codeToValidate[i] = digit;
+
+        if (i === pastedCode.length - 1) {
+          (this.inputs[i + 1] || this.inputs[i]).disabled = false;
+          (this.inputs[i + 1] || this.inputs[i]).focus();
+        } else {
+          this.inputs[i].disabled = true;
+        }
+      });
+    }
+  }
+};
+
+TwoFactorCodeInput.prototype.getValidCode = function () {
+  return this.validCode;
+};
+
+TwoFactorCodeInput.prototype.validateCode = function () {
+  return this.validCode == this.codeToValidate.join("");
+};
